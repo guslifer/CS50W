f82a37e574651509ffbc379346a4e7159e07fc77
guslifer <gustavolinofernandes@gmail.com>
2022-06-12 12:33:38 -0300

    Structure: Defining models
    Defining base models to start working on, migrations already applied.

--- web50/projects/2020/x/commerce/auctions/admin.py
+++ web50/projects/2020/x/commerce/auctions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Listings, Bids, Comments, Categories, User
 
 # Register your models here.
+
+class BidsAdmin(admin.ModelAdmin):
+    exclude = ('id',)
+    
+class ListingsAdmin(admin.ModelAdmin):
+    exclude = ('id',)
+    
+class CommmentsAdmin(admin.ModelAdmin):
+    exclude = ('id',)
+
+class CategoriesAdmin(admin.ModelAdmin):
+    exclude = ('id',)
+
+admin.site.register(User)
+admin.site.register(Listings,ListingsAdmin)
+admin.site.register(Bids,BidsAdmin)
+admin.site.register(Comments,CommmentsAdmin)
+admin.site.register(Categories,CategoriesAdmin)
--- web50/projects/2020/x/commerce/auctions/models.py
+++ web50/projects/2020/x/commerce/auctions/models.py
@@ -1,6 +1,45 @@
+from tkinter import CASCADE
+from unicodedata import category
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.forms import CharField, DateTimeField, FloatField, IntegerField
 
 
 class User(AbstractUser):
     pass
+
+class Categories(models.Model):
+    id = models.IntegerField(primary_key = True)
+    category = models.CharField(max_length=100)
+
+class Listings(models.Model):
+    ACTIVE = 1
+    CANCELLED = 2
+    SOLD = 3
+    STATUS_CHOICES = (
+        (ACTIVE, 'Active'),
+        (CANCELLED, 'Cancelled'),
+        (SOLD, 'Sold'),
+    )
+    
+    id = models.IntegerField(primary_key = True)
+    product_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    base_price = models.FloatField()
+    image_url = models.URLField()
+    category = models.ForeignKey(Categories, related_name="listinings",on_delete=CASCADE,null=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default = ACTIVE)
+    
+
+class Bids(models.Model):
+    id = models.IntegerField(primary_key = True)
+    price = models.FloatField()
+    author = models.ForeignKey(User, related_name="bids", on_delete=CASCADE,null=True)
+    
+class Comments(models.Model):
+    comment = models.CharField(max_length=500)
+    author = models.ForeignKey(User, related_name="comments",null=True,on_delete=CASCADE)
+    
+    
+
