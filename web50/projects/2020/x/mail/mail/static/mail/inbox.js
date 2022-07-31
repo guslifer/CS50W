468026c88b6a9f9849e3a455b961cf2f007a1d66
guslifer <gustavolinofernandes@gmail.com>
2022-07-31 18:59:27 -0300

    feat: send email
    Send email via compose view

--- web50/projects/2020/x/mail/mail/static/mail/inbox.js
+++ web50/projects/2020/x/mail/mail/static/mail/inbox.js
@@ -6,10 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  document.querySelector("#submit_email").addEventListener('click', () => {
+    let recipients = document.getElementById("compose-recipients").value;
+    let body = document.getElementById("compose-body").value;
+    let subject = document.getElementById("compose-subject").value;
+    send_email(recipients, subject, body);
+  });
+  
+
   // By default, load the inbox
   load_mailbox('inbox');
-});
-
+  });
+  
 function compose_email() {
 
   // Show compose view and hide other views
@@ -20,6 +28,7 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +39,13 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(c_recipient, c_subject, c_body){
+  fetch('/emails', {method: 'POST', body: JSON.stringify({
+    recipients: c_recipient,
+    subject: c_subject,
+    body: c_body })
+   });
+
 }
